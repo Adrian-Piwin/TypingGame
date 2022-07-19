ce757f4f902185448b65dc58b2ff9f00aa66d9de
Adrian Piwin <APiwin@mysysltd.com>
2022-07-19 16:59:11 -0400

    Added feedback when pressing a key Added exiting mid game

--- script.js
+++ script.js
@@ -1,65 +1,179 @@
-var keyList = [
-    'q','w','e','r','t','y','u','i','o','p',
-    'a','s','d','f','g','h','j','k','l',';',
-    'z','x','c','v','b','n','m',',','.','/'
-]
+window.addEventListener("load", function(event) {
+    /* ==== REFERENCES ====*/ 
 
-var selectedKeyList = []
+    var keyElements = document.getElementsByClassName('key');
+    var textPrompt = document.getElementById('textPrompt');
 
-var gameRunnning = false;
+    /* ==== GAME VARIABLES ====*/ 
 
-var keyElements = document.getElementsByClassName('key');
+    var keyList = [
+        'q','w','e','r','t','y','u','i','o','p',
+        'a','s','d','f','g','h','j','k','l',';',
+        'z','x','c','v','b','n','m',',','.','/'
+    ]
 
+    var selectedKeyList = []
+    var gameRunnning = false;
+    var isTyping = false;
 
-/* ==== GET KEY AND VERIFY VALID ====*/ 
-document.onkeydown = function (e) {
-    verifyInput(e.key);
-};
+    /* ==== VARIABLES ====*/ 
 
-function verifyInput(key){
-    if (keyList.includes(key) || key == 'Enter')
-        keyPressed(key);
-}
+    var startingStr = 'press enter'
+    var lostStr = 'you lose'
 
-/* ==== GAME ====*/ 
+    var selectKeyInterval = 1000;
+    var typePromptInterval = 100;
 
-function keyPressed(key){
-    if (!gameRunnning && key == 'Enter')
-        startGame();
-    else{
+    /* ==== GET KEY AND VERIFY VALID ====*/ 
 
+    document.onkeydown = function (e) {
+        VerifyInput(e.key);
+    };
+
+    function VerifyInput(key){
+        if (keyList.includes(key) || key == 'Enter')
+            KeyPressed(key);
     }
-}
 
-function startGame(){
+    /* ==== GAME ====*/ 
 
-}
+    // Start game on enter
+    // Handle key input when playing game
+    function KeyPressed(key){
+        if (!gameRunnning && key == 'Enter'){
+            isTyping = false;
+            gameRunnning = true;
+            textPrompt.innerHTML = '';
+            ResetKeys();
+            Game();
+        }
+        else if (gameRunnning && key == 'Enter'){
+            isTyping = false;
+            gameRunnning = false;
+            EnterPromptText(startingStr);
+            ResetKeys();
+        }
+        else if (gameRunnning){
+            DeselectKey(key);
+        }
 
-function selectKey(){
-    // Get random key
-    let selKey = keyList[Math.floor(Math.random() * keyList.length)];
+        PressKey(key);
+    }
 
-    selectedKeyList.push(selKey);
-    let selKeyElm = selectKeyElem(selKey);
-    console.log(selKeyElm);
-    if (selKeyElm != null)
-        selKeyElm.classList.add('selected');
-}
+    // Handle starting game
+    function Game(){
+        if (gameRunnning == false) return;
 
-function selectKeyElem(key){
+        SelectKey();
 
-    for (i = 0; i < keyElements.length; i++){
-        console.log(keyElements.item(i).innerHTML);
+        setTimeout(function () {
+            Game();
+        }, selectKeyInterval);
+    }
+    
+    // Handle losing game
+    function Lose(){
+        EnterPromptText(lostStr);
+        gameRunnning = false;
+    }
 
-        if (keyElements.item(i).innerHTML == key.toUpperCase()){
-            return keyElements[i]
+    function ResetKeys(){
+        // Unselect all keys
+        for (i = 0; i < keyElements.length; i++){
+            keyElements.item(i).classList.remove('selected');
+        }
+
+        // Clear list
+        selectedKeyList = [];
+    }
+
+    /* ==== TEXT FUNCTIONS ====*/ 
+
+    // Enter text letter by letter, with an interval
+    function EnterPromptText(text, textIndex=0){
+        if (textIndex == 0)
+            isTyping = true;
+        if (text.length == textIndex) {
+            isTyping = false;
+            return;
+        }
+        if (!isTyping){
+            textPrompt.innerHTML = '';
+            return;
+        }
+
+        let newText = textPrompt.innerHTML;
+        newText += text[textIndex];
+        textPrompt.innerHTML = newText;
+
+        setTimeout(function () {
+            EnterPromptText(text, textIndex+1);
+        }, typePromptInterval);
+    }
+
+    /* ==== KEY FUNCTIONS ====*/ 
+
+    function SelectKey(){
+        // Filter already selected keys from key list
+        var newKeyList = filterArray(keyList, selectedKeyList);
+
+        // If all keys selected, lose
+        if (newKeyList.length == 0){
+            Lose();
+        }
+        
+        // Select random key  
+        let selKey = newKeyList[Math.floor(Math.random() * newKeyList.length)];
+
+        // Add selected class to key
+        selectedKeyList.push(selKey);
+        let selKeyElm = FindKeyElem(selKey);
+        if (selKeyElm != null)
+            selKeyElm.classList.add('selected');
+    }
+
+    function DeselectKey(key){
+        // Remove key from list
+        selectedKeyList = selectedKeyList.filter(e => e !== key);
+
+        // Remove selected class from key
+        let keyElm = FindKeyElem(key);
+        if (keyElm != null)
+            keyElm.classList.remove('selected');
+    }
+
+    function PressKey(key){
+        // Show effect for pressing key
+        let keyElm = FindKeyElem(key);
+        if (keyElm != null){
+            keyElm.classList.add('pressed');
+            setTimeout(function () {
+                keyElm.classList.remove('pressed');
+            }, 250);
         }
     }
 
-    return null;
-}
+    // Returns key element from key string
+    // Returns null if key not found
+    function FindKeyElem(key){
+        for (i = 0; i < keyElements.length; i++){
+            if (keyElements.item(i).innerHTML == key.toUpperCase()){
+                return keyElements[i]
+            }
+        }
 
-window.addEventListener("load", function(event) {
-    selectKey();
-  
+        return null;
+    }
+
+    /* ==== UTILITY FUNCTIONS ====*/ 
+
+    // Filter array from another
+    const filterArray = (arr1, arr2) => {
+        const filtered = arr1.filter(el => {
+        return arr2.indexOf(el) === -1;
+        });
+        return filtered;
+    };
+
+    EnterPromptText(startingStr);
 });
